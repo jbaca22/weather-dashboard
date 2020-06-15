1571c3a643b494586e934e688ed28b252ef41ed3
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-06-14 23:54:39 -0600

    finished weather data, working on forecasts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,36 +2,51 @@ const apikey = "e931caab6a9b47e29d705130201506&";
 var city = {};
 
 $("#start-search").on("click", function() {
-    city = $("#input").val();
-    console.log(city);
-    $("#city-name").append(location.name);
     getCityInfo();
 })
 
-fetch("https://api.weatherapi.com/v1/current.json?key=e931caab6a9b47e29d705130201506&q=city").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
-
-
-
 var getCityInfo = function () {
-    var date = {};
-    var temp = {};
-    var humidity = {};
-    var windspeed = {};
-    var UV = {};
-    var store = {};
+    city = $("#input").val();
 
     var apiurl = "https://api.weatherapi.com/v1/current.json?key=" + apikey +"q=" +city;
-        fetch(apiurl).then(function(response) {
-            response.json().then(function(data) {
-            console.log(data,);
-        });
-    });
 
-    setStorage();
+    $.ajax({
+        url: apiurl,
+        method: "GET"
+      })
+      .then(function(response){
+        console.log(response);
+        console.log(response.current.temp_f);
+
+        currentWeather(response);
+        forecast(response);
+
+        setStorage();
+      })
+}
+
+var currentWeather = function (response){
+    console.log(response);
+    $("#city-name").append(response.location.name + ":   " + response.location.localtime);
+    $("#temp").text("Temperature:  ").append(response.current.temp_f);
+    $("#humidity").text("Humidity:  ").append(response.current.humidity);
+    $("#wind-speed").text("Wind Speed:  ").append(response.current.wind_mph);
+    $("#uv-index").text("Uv Index:  ").append(response.current.uv);
+}
+
+var forecast = function (response){
+    var forecastURL ="https://api.weatherapi.com/v1/forecast.json?key=" +apikey +"q=" +city;
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+        console.log(response.forecast.forecastday[0].day.avgtemp_f);
+        $("#list1").append(response.forecast.forecastday[0].date);
+        $("#list2").text("temperature:  ").append(response.forecast.forecastday[0].day.avgtemp_f);
+        $("#list3").text("Humidity:  ").append(response.forecast.forecastday[0].day.avghumidity);
+    })
 }
 
 
